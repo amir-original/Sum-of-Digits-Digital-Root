13f6063ab4e0d96479600fa43a2c3005a172b474
amir-original <amirrahmani7017@gmail.com>
2022-07-15 07:15:19 +0430

    refactor

--- src/main/java/DigitalRoot.java
+++ src/main/java/DigitalRoot.java
@@ -1,20 +1,41 @@
 public class DigitalRoot {
-    public int sumDigitalRoot(int number) {
-        String root = String.valueOf(number);
-        int sum = 0;
-        while (root.length() != 1) {
-            sum = sumRoots(root);
-            root = String.valueOf(sum);
-        }
 
-        return sum;
+    private static final int ONE_DIGIT = 1;
+
+    public int sumDigitalRoot(int number) {
+        String root = intToString(number);
+        int sum = sumRoots(root);
+
+        if (hasOneDigit(root)) return sum;
+
+        return sumDigitalRoot(sum);
     }
 
-    private int sumRoots(String root) {
-        int sum = 0;
-        for (int i = 0; i < root.length(); i++) {
-            sum += Integer.parseInt(String.valueOf(root.charAt(i)));
-        }
-        return sum;
+    private boolean hasOneDigit(String root) {
+        return root.length() == ONE_DIGIT;
+    }
+
+    private int sumRoots(String number) {
+        int result = 0;
+        for (int index = 0; index < number.length(); index++)
+            result += getDigitalRoot(number, index);
+
+        return result;
+    }
+
+    private int getDigitalRoot(String number, int index) {
+        return toInteger(charToString(number.charAt(index)));
+    }
+
+    private int toInteger(String number) {
+        return Integer.parseInt(number);
+    }
+
+    private String charToString(char number) {
+        return String.valueOf(number);
+    }
+
+    private String intToString(int number) {
+        return String.valueOf(number);
     }
 }
