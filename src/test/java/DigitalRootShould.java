46e8566a89a237a81836ba0ce4abd437735694a2
amir-original <amirrahmani7017@gmail.com>
2022-07-15 07:19:45 +0430

    refactor

--- src/test/java/DigitalRootShould.java
+++ src/test/java/DigitalRootShould.java
@@ -6,17 +6,14 @@ public class DigitalRootShould {
 
     @Test
     void take_the_sum_of_the_digits_of_number_when_given_number() {
-
         final DigitalRoot digitalRoot = new DigitalRoot();
-
         assertThat(digitalRoot.sumDigitalRoot(16)).isEqualTo(7);
         assertThat(digitalRoot.sumDigitalRoot(27)).isEqualTo(9);
         assertThat(digitalRoot.sumDigitalRoot(42)).isEqualTo(6);
-
         assertThat(digitalRoot.sumDigitalRoot(82)).isEqualTo(1);
+        assertThat(digitalRoot.sumDigitalRoot(456)).isEqualTo(6);
         assertThat(digitalRoot.sumDigitalRoot(942)).isEqualTo(6);
         assertThat(digitalRoot.sumDigitalRoot(132189)).isEqualTo(6);
         assertThat(digitalRoot.sumDigitalRoot(493193)).isEqualTo(2);
-
     }
 }
